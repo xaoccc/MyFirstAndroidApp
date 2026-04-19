3a0162a2fc49ae6652e913b934952863e1661a8a
xaoccc <114498517+xaoccc@users.noreply.github.com>
2026-04-19 19:26:59 +0300

    first java android intent

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.myfirstandroidapp;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeText(View v) {
         ((Button) v).setText("Text Changed!");
+    }
+
+    public void loginPage(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
     }
 }
 
