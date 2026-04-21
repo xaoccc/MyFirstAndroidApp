3c5f36f751df02e5f785d43bd5bdfb36f8250ec6
xaoccc <xaocccc@gmail.com>
2026-04-21 13:10:53 +0300

    register user and login user - success!

--- app/src/main/java/com/example/myfirstandroidapp/LoginActivity.java
+++ app/src/main/java/com/example/myfirstandroidapp/LoginActivity.java
@@ -1,31 +1,68 @@
 package com.example.myfirstandroidapp;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText nameInput;
+    private EditText userNameInput;
+    private EditText passwordInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        nameInput = findViewById(R.id.userNameInput);
+        userNameInput = findViewById(R.id.userNameInput);
+        passwordInput = findViewById(R.id.passwordInput);
 
     }
 
     public void submit(View v) {
-        Intent intent = new Intent(this, AppActivity.class);
-        String name = nameInput.getText().toString();
+        SharedPreferences prefs;
 
-        //  Pass the name to the AppActivity
-        intent.putExtra("USERNAME", name);
-        startActivity(intent);
+        try {
+            MasterKey masterKey = new MasterKey.Builder(this)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            prefs = EncryptedSharedPreferences.create(
+                    this,
+                    "secure_prefs", // SAME NAME
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        String savedUsername = prefs.getString("username", null);
+        String savedPassword = prefs.getString("password", null);
+
+        String inputUsername = userNameInput.getText().toString();
+        String inputPassword = passwordInput.getText().toString();
+
+        if (savedUsername != null && savedPassword != null &&
+                savedUsername.equals(inputUsername) &&
+                savedPassword.equals(inputPassword)) {
+
+            Intent intent = new Intent(this, AppActivity.class);
+
+            //  Pass the name to the AppActivity
+            intent.putExtra("USERNAME", inputUsername);
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
